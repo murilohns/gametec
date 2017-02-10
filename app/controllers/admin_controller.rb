c1202c8d0a9d7d663813e8b759e27e756266a6ec
Murilo Henrique <murilohns@gmail.com>
2017-02-10 15:30:28 +0000

    Changed top-bar

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,10 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   
   layout 'admin'
+
+  def index
+  end
+
+  def signup
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,16 @@ Rails.application.routes.draw do
 
   get 'news', to: 'news#index'
 
-  get 'home/show_notice/:id' => 'home#show_notice'
-  
   resources :materials, only: [:new, :create, :destroy]
   
   resources :notices, except:[:index, :show]
-
   
   devise_for :admins
 
   get 'home/index'
+
   get 'admin', to: 'admin#index'
+  get 'admin/signup'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
