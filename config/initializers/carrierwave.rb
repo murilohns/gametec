478b75302bde244f2831624e8ccc411f82385405
Murilo Henrique <murilohns@gmail.com>
2017-04-20 19:38:02 +0000

    Trying to put files in aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,34 +1,22 @@
-# config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    # Configuration for Amazon S3 should be made available through an Environment variable.
-    # For local installations, export the env variable through the shell OR
-    # if using Passenger, set an Apache environment variable.
-    #
-    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-    #
-    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
+  config.storage    = :aws
+  config.aws_bucket = 'gametec-files'
+  config.aws_acl    = 'public-read'
 
-    # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => ENV['AWS_REGION']
+  # Optionally define an asset host for configurations that are fronted by a
+  # content host, such as CloudFront.
+  config.asset_host = 's3_domain_url'
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:            ENV.fetch('AWS_REGION') # Required
   }
 
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+  # Optional: Signing of download urls, e.g. for serving private content through
+  # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
+  # configured:
+  # config.aws_signer = -> (unsigned_url, options) do
+  #   Aws::CF::Signer.sign_url(unsigned_url, options)
+  # end
 end
