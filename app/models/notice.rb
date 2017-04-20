7c59da8104f954c1cfa178726fe2ed54847a1f74
Murilo Henrique <murilohns@gmail.com>
2017-04-20 16:52:26 +0000

    Trying to put images into aws

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -2,16 +2,8 @@ class Notice < ApplicationRecord
   belongs_to :admin, optional: true
 
   has_attached_file :image,
-  					styles:{thumb:["700x450#",:jpg]},
-    
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
-
+  					styles:{thumb:["700x450#",:jpg]}
+            
   validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
